ca6ec42baa2f095fe22909d60bd4564db04f2bef
Дмитро <124165148+sd13666@users.noreply.github.com>
2024-05-14 20:51:24 +0300

    fixed timeline in monthes

--- scripts/calendar/timeline.js
+++ scripts/calendar/timeline.js
@@ -1,3 +1,50 @@
+// export const timeLine = () => {
+//   const existingTimeLine = document.querySelector('.time-line');
+//   if (existingTimeLine) {
+//     existingTimeLine.remove();
+//   }
+
+//   const curTimeEl = document.createElement('div');
+//   curTimeEl.className = 'time-line';
+
+//   const circleEl = document.createElement('div');
+//   circleEl.className = 'time-line__circle';
+//   curTimeEl.appendChild(circleEl);
+
+//   const rectangleEl = document.createElement('div');
+//   rectangleEl.className = 'time-line__rectangle';
+//   curTimeEl.appendChild(rectangleEl);
+//   const timeSlots = document.querySelectorAll('.calendar__time-slot');
+
+//   const currentTime = new Date();
+//   const currentHour = currentTime.getHours();
+//   const currentMinute = currentTime.getMinutes();
+//   const currentDay = currentTime.getDate();
+
+//   curTimeEl.dataset.time = currentHour;
+//   curTimeEl.dataset.day = currentDay;
+//   curTimeEl.style.left = '-3px';
+//   curTimeEl.style.top = `${currentMinute - 2}px`;
+
+//   let selectedIndex = -1;
+
+//   timeSlots.forEach((slot, index) => {
+//     const slotTime = parseInt(slot.dataset.time, 10);
+//     const slotDay = parseInt(slot.parentElement.dataset.day, 10);
+
+//     if (slotTime === currentHour && slotDay === currentDay) {
+//       selectedIndex = index;
+//     }
+//   });
+
+//   if (selectedIndex !== -1) {
+//     timeSlots[selectedIndex].append(curTimeEl);
+//   }
+// };
+
+// setInterval(timeLine, 60 * 1000);
+
+
 export const timeLine = () => {
   const existingTimeLine = document.querySelector('.time-line');
   if (existingTimeLine) {
@@ -20,6 +67,7 @@ export const timeLine = () => {
   const currentHour = currentTime.getHours();
   const currentMinute = currentTime.getMinutes();
   const currentDay = currentTime.getDate();
+  const currentMonth = currentTime.getMonth() + 1; // Отримайте поточний місяць
 
   curTimeEl.dataset.time = currentHour;
   curTimeEl.dataset.day = currentDay;
@@ -31,8 +79,9 @@ export const timeLine = () => {
   timeSlots.forEach((slot, index) => {
     const slotTime = parseInt(slot.dataset.time, 10);
     const slotDay = parseInt(slot.parentElement.dataset.day, 10);
+    const slotMonth = parseInt(slot.parentElement.dataset.month, 10); // Отримайте місяць
 
-    if (slotTime === currentHour && slotDay === currentDay) {
+    if (slotTime === currentHour && slotDay === currentDay && slotMonth === currentMonth) {
       selectedIndex = index;
     }
   });
@@ -41,5 +90,3 @@ export const timeLine = () => {
     timeSlots[selectedIndex].append(curTimeEl);
   }
 };
-
-setInterval(timeLine, 60 * 1000);
