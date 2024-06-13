0078be83d2f799a76ce4ac7caec0f5652057e0e8
dmitro-utkin <sssddd1366628@gmail.com>
2024-06-13 15:53:17 +0300

    fixed eventsColors

--- scripts/events/eventsColor.js
+++ scripts/events/eventsColor.js
@@ -1,45 +1,29 @@
 import { getItem } from '../common/storage.js';
 import { updateEventColor } from '../common/gateways.js';
 import { renderEvents } from './events.js';
-import { closePopup } from "../common/popup.js";
+import { closePopup } from '../common/popup.js';
 
 export const handleColorButtonClick = () => {
   const colorButtons = document.querySelectorAll('.colors__item');
 
-  const onClick = (event) => {
-    console.log('Color button clicked');
-
+  const onClick = async (event) => {
+    const color = getComputedStyle(event.target).backgroundColor;
     const eventId = getItem('eventIdToDelete');
-    const style = getComputedStyle(event.target);
-    const color = style.backgroundColor;
 
-    console.log('Event ID:', eventId);
-    console.log('Color:', color);
-
-    updateEventColor(+eventId, color)
-      .then(() => {
-        console.log('Event color updated successfully');
-
-        const eventsToUpdate = document.querySelectorAll(
-          `.event[data-event-id="${eventId}"]`
-        );
-        eventsToUpdate.forEach(eventToUpdate => {
-          eventToUpdate.style.setProperty('background-color', color);
+    try {
+      await updateEventColor(+eventId, color);
+      document
+        .querySelectorAll(`.event[data-event-id="${eventId}"]`)
+        .forEach((eventToUpdate) => {
+          eventToUpdate.style.backgroundColor = color;
         });
-        console.log('Events updated:', eventsToUpdate);
-
-        closePopup();
-        console.log('Popup closed');
-
-        renderEvents();
-        console.log('Events rendered');
-      })
-      .catch(error => {
-        console.error('Failed to update the event color:', error);
-      });
+    } catch (error) {
+      console.error('Failed to update the event color:', error);
+    } finally {
+      closePopup();
+      renderEvents();
+    }
   };
 
-  colorButtons.forEach(button => button.addEventListener('click', onClick));
+  colorButtons.forEach((button) => button.addEventListener('click', onClick));
 };
-
-
