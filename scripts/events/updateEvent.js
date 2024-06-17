1a7e0dcef3bb7fe4ed208824e13046695c0013df
dmitro-utkin <sssddd1366628@gmail.com>
2024-06-17 18:08:36 +0300

    solved problem with css

--- scripts/events/updateEvent.js
+++ scripts/events/updateEvent.js
@@ -1,10 +1,7 @@
 import { getItem } from '../common/storage.js';
-import { updateEvent, serverUrl } from '../common/gateways.js';
+import { serverUrl } from '../common/gateways.js';
 import { closePopup } from '../common/popup.js';
-import { openModal, closeModal } from '../common/modal.js';
-import { getDateTime } from '../common/utils.js';
-import { renderEvents } from './events.js';
-import { onCloseEventForm } from './createEvent.js';
+import { openModal } from '../common/modal.js';
 
 const editEventBtn = document.querySelector('.events-btn__edit-btn');
 
@@ -51,39 +48,39 @@ export const onEventUpdate = async () => {
   openModal();
   fillForm(event);
 
-  const form = document.querySelector('.event-form');
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault();
+  // const form = document.querySelector('.event-form');
+  // form.addEventListener('submit', async (event) => {
+  //   event.preventDefault();
 
-    console.log('Form submit event');
+  //   console.log('Form submit event');
 
-    const formData = new FormData(form);
-    const { startTime, endTime, date, ...rest } = Object.fromEntries(
-      formData.entries()
-    );
-    console.log('Form data:', rest);
+  //   const formData = new FormData(form);
+  //   const { startTime, endTime, date, ...rest } = Object.fromEntries(
+  //     formData.entries()
+  //   );
+  //   console.log('Form data:', rest);
 
-    const eventDetails = {
-      ...rest,
-      date,
-      start: getDateTime(date, startTime),
-      end: getDateTime(date, endTime)
-    };
-    console.log('Event details:', eventDetails);
+  //   const eventDetails = {
+  //     ...rest,
+  //     date,
+  //     start: getDateTime(date, startTime),
+  //     end: getDateTime(date, endTime)
+  //   };
+  //   console.log('Event details:', eventDetails);
 
-    const response = await updateEvent(eventIdToUpdate, {
-      ...event,
-      ...eventDetails
-    });
+  //   const response = await updateEvent(eventIdToUpdate, {
+  //     ...event,
+  //     ...eventDetails
+  //   });
 
-    if (response.ok) {
-      console.log('Event updated successfully');
-      await renderEvents();
-      closeModal();
-    } else {
-      console.error('Failed to update the event:', response.statusText);
-    }
-  });
+  //   if (response.ok) {
+  //     console.log('Event updated successfully');
+  //     await renderEvents();
+  //     closeModal();
+  //   } else {
+  //     console.error('Failed to update the event:', response.statusText);
+  //   }
+  // });
 };
 
 export const initEditEvent = () => {
