f4cdf0c5783ac517bb0b0c41bd0360b51c3083ae
dmitro-utkin <sssddd1366628@gmail.com>
2024-06-21 13:35:23 +0300

    fixed with prettier

--- scripts/common/utils.js
+++ scripts/common/utils.js
@@ -5,10 +5,7 @@ export const daysOfWeek = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 export const getStartOfWeek = date => {
   const dateCopy = new Date(date);
   const dayOfWeek = dateCopy.getDay();
-  const difference =
-    dayOfWeek === 0
-      ? -6
-      : 1 - dayOfWeek;
+  const difference = dayOfWeek === 0 ? -6 : 1 - dayOfWeek;
   const monday = new Date(dateCopy.setDate(dateCopy.getDate() + difference));
   return new Date(monday.getFullYear(), monday.getMonth(), monday.getDate());
 };
