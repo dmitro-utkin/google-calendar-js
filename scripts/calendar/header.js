f9abd4d4b76d3f90190c4f128424f47bf405c80c
dmitro-utkin <sssddd1366628@gmail.com>
2024-06-21 13:39:00 +0300

    fixed prettierrc

--- scripts/calendar/header.js
+++ scripts/calendar/header.js
@@ -33,31 +33,3 @@ document.querySelector('.create-event-btn').addEventListener('click', () => {
   formSubmitBtn.textContent = 'Create';
   formSubmitBtn.addEventListener('click', handleCreate, { once: true });
 });
-
-// export const renderHeader = () => {
-//   const displayedWeekStart = getItem('displayedWeekStart');
-//     const weekDays = generateWeekRange(displayedWeekStart);
-//     let weekHTML = '';
-
-//     const today = new Date();
-
-//     weekDays.forEach(day => {
-//         const dayOfWeek = daysOfWeek[day.getDay()];
-//         const dayOfMonth = day.getDate();
-
-//         const isToday = day.toDateString() === today.toDateString();
-//         const todayNameClass = isToday ? 'today-name' : '';
-//         const todayDayClass = isToday ? 'today-number' : '';
-
-//         weekHTML += `<div class="calendar__day-label day-label">
-//             <span class="day-label__day-name ${todayNameClass}">${dayOfWeek}</span>
-//             <span class="day-label__day-number  ${todayDayClass}">${dayOfMonth}</span>
-//         </div>`;
-//     });
-//     document.querySelector('.calendar__header').innerHTML = weekHTML;
-// };
-
-// document.querySelector('.create-event-btn').addEventListener('click', () => {
-//   openModal();
-//   document.querySelector(".event-form__submit-btn").textContent = "Create";
-// });
